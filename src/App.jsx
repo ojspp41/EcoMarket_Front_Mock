b3f0b526f20e0f3e663fab2b33536995f31c10a3
BackG <jw6133@naver.com>
2024-10-28 07:07:24 +0000

    feat : uploadList new

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Register from './pages/Register';
-import Auction from './pages/Auction';
-import Profile from './pages/Profile';
-import Navbar from './components/Navbar';
-import LoginPage from './pages/LoginPage';
-import GlobalStyle from './css/GlobalStyle';
-import UploadThings from './pages/UploadThings';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Register from "./pages/Register";
+import Auction from "./pages/Auction";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import GlobalStyle from "./css/GlobalStyle";
+import UploadThings from "./pages/UploadThings";
+import UploadList from "./pages/UploadList";
 function App() {
-  
-
   return (
     <>
-      <GlobalStyle/>
+      <GlobalStyle />
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/login" element={<LoginPage/>} /> 
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<Register />} />
             <Route path="/" element={<Auction />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/uploadlist" element={<UploadList />} />
             <Route path="/upload" element={<UploadThings />} />
           </Routes>
           <Navbar /> {/* 항상 하단에 고정되는 Navbar */}
         </div>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
